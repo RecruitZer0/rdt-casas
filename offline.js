2c5bdea9ff89bc5dea40b9d72aff397ad50db323
Tadeudosgames <tadeudosgamesOFICIAL@gmail.com>
2022-04-01 23:21:04 -0300

    upload

--- offline.js
+++ offline.js
@@ -1,10 +1,9 @@
 ﻿{
-	"version": 1648865226,
+	"version": 1648865960,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
-		"offlineClient.js",
-		"keys.json"
+		"offlineClient.js"
 	]
 }
